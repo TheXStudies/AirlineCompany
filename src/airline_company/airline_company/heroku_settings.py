87f02d9b1a94f05952aa2dc79c1869cb25176916
Andrey Y <thexstudies@gmail.com>
2020-04-01 20:37:46 +0300

    Squashed commit of the following:
    commit 217a60658c6eb9c6cf8255d68900edcb69effb35
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 20:36:36 2020 +0300
    
        Parse heroku URL
    
    commit e4b048f4288606c6dbf7987652984acb5c6531a0
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 11:48:51 2020 +0300
    
        [heroku] Fix path to base.css
    
    commit 903f71b2598649c77788c155294c6b19da3b5f4c
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 11:45:21 2020 +0300
    
        [heroku] Serve static files from urls.py
    
    commit 93e5b4c4623cdec464df02c9584697521b0a11a8
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 11:38:31 2020 +0300
    
        [heroku] Remove tmp line in requirements
    
    commit 9f0da338b61b04128ccf27642aa80f567020c9ad
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 11:35:54 2020 +0300
    
        [heroku] Enable DEBUG
    
    commit 9c76b44f8aea1d2efda0a4ca1a256e7ccd3fb77e
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 11:30:09 2020 +0300
    
        [heroku] Add whitenoise usage
    
    commit f276945726a0d4a855e0bf69cea37c1601e1e59c
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 10:56:25 2020 +0300
    
        [heroku] Add gunicorn dependency
    
    commit b5737fe8e7b08e489575fe17ccca86be4a7922cb
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 10:51:12 2020 +0300
    
        [heroku] Add psycopg2 dependency
    
    commit 91554749db0243209bc3a9e91763a003c91b8790
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 10:49:47 2020 +0300
    
        [heroku] Set database properly
    
    commit a640796c6e164d8990937e04c6417b73f43be7d1
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 10:46:28 2020 +0300
    
        [heroku] Enable DEBUG
    
    commit 4c69c64069dd5020cbfe62e3fed7917d5519c83b
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 10:43:58 2020 +0300
    
        [heroku] add heroku_settings
    
    commit 9109b6eca3056f5482a18b5faae315d885cd750b
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 10:38:09 2020 +0300
    
        [heroku] Use wsgi instead of asgi
    
    commit 0740d671cd6e243652c67288edd993922a9a1785
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 10:32:45 2020 +0300
    
        airlinecompany-airline_company in Procfile
    
    commit d713eacce192bc3ab1973a3647d88ba2a6424fc7
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 10:29:21 2020 +0300
    
        Set STATIC_ROOT to be inside project path
    
    commit 51355bc07edf3424a43af6ac4b18affe0f491b19
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 10:27:21 2020 +0300
    
        Add usage of static files
    
    commit 68ef1cc7bf472c2d14deb9383c555cc47e459e7e
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 10:24:17 2020 +0300
    
        Add STATICFILES_DIRS setting
    
    commit 79ecee0224a7d41c699c64f0f931b310730f5c99
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Wed Apr 1 10:19:50 2020 +0300
    
        Update requirements.txt
    
    commit dd5c613104491f11bf2cf6ad05d5c79d34de5cff
    Author: Andrey Y <thexstudies@gmail.com>
    Date:   Tue Mar 31 12:57:31 2020 +0300
    
        Add requirements.txt and Procfile

---
+++ src/airline_company/airline_company/heroku_settings.py
@@ -0,0 +1,21 @@
+from .settings import *
+from urllib.parse import urlparse
+
+db_url = urlparse(os.environ.get('DATABASE_URL'))
+
+# DEBUG = False
+ALLOWED_HOSTS = ['.herokuapp.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': db_url.hostname,
+        'PORT': db_url.port,
+        'USER': db_url.username,
+        'PASSWORD': db_url.password,
+        'NAME': db_url.path[1:],
+    },
+}
+
+MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
